85edda6b10a4e1e4da49834a2194ba876fe13725
PANDIT <87899750+Riteshp2001@users.noreply.github.com>
2023-08-01 23:47:46 +0530

    Added Delete and LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,33 @@ import { useState, useEffect } from "react";
 function App() {
 	const [tasks, setTasks] = useState([]);
 
+	useEffect(() => {
+		setTasks(JSON.parse(localStorage.getItem("tasks")));
+	}, []);
+
 	const handleAddTask = (taskText, day) => {
 		const currentDate = new Date().toLocaleDateString();
 		const minitask = {
 			time: new Date().toLocaleTimeString(),
 			text: taskText,
 			taskId: (Math.random() + 1000).toString(),
+			isEditable: false,
+			strike: false,
 		};
+
 		if (currentDate === day && tasks.length !== 0) {
 			setTasks((prevTasks) => {
-				const newTasks = [...prevTasks];
-				newTasks.forEach((task) => {
+				const newTasks = prevTasks.map((task) => {
 					if (task.day === currentDate) {
-						task.weekdayTasks.push(minitask);
+						return {
+							...task,
+							weekdayTasks: [...task.weekdayTasks, minitask],
+						};
 					}
+					return task;
 				});
+
+				localStorage.setItem("tasks", JSON.stringify(newTasks));
 				return newTasks;
 			});
 		} else {
@@ -28,6 +40,7 @@ function App() {
 				weekdayTasks: [minitask],
 			};
 
+			localStorage.setItem("tasks", JSON.stringify(newTask));
 			setTasks((prevTasks) => [...prevTasks, newTask]);
 		}
 	};
@@ -38,7 +51,7 @@ function App() {
 			<Time />
 			<div className="task-container">
 				<TextArea onAddTask={handleAddTask} />
-				<DayTasks tasks={tasks} />
+				<DayTasks tasks={tasks} setTasks={setTasks} />
 			</div>
 		</div>
 	);
@@ -78,31 +91,45 @@ function Time() {
 }
 
 function TextArea({ onAddTask }) {
-	const handleButtonClick = () => {
-		let taskInput = document.getElementById("txt");
-		const taskText = taskInput.value.trim();
-		const day = new Date().toLocaleDateString();
+	let [inpVal, setInpVal] = useState("");
 
-		if (taskText !== "") {
-			console.log("task added");
-			onAddTask(taskText, day);
-			taskInput.value = "";
-		}
-	};
+	function handleSubmit(event) {
+		event.preventDefault();
+		onAddTask(inpVal, new Date().toLocaleDateString());
+		setInpVal("");
+	}
+
+	function setVal(event) {
+		setInpVal((inpVal) => {
+			inpVal = event.target.value;
+			return inpVal;
+		});
+	}
 
 	return (
-		<div className="add-list">
-			<input type="text" name="text-row" id="txt" placeholder="Enter your Task" className="list" />
-			<button type="button" value="Add +" id="addbtn" onClick={handleButtonClick}>
-				Add+
-			</button>
-		</div>
+		<form action="" onSubmit={handleSubmit}>
+			<div className="add-list">
+				<input
+					type="text"
+					name="text-row"
+					id="txt"
+					placeholder="Enter your Task"
+					className="list"
+					onChange={setVal}
+					autoFocus={true}
+					value={inpVal}
+				/>
+				<button type="button" value="Add +" id="addbtn" onClick={handleSubmit}>
+					Add+
+				</button>
+			</div>
+		</form>
 	);
 }
 
-function DayTasks({ tasks }) {
+function DayTasks({ tasks, setTasks }) {
 	return (
-		<div key={tasks.id}>
+		<>
 			{tasks.map((task) => (
 				<div className="newdate" key={task.date}>
 					<h2>{task.weekday}</h2>
@@ -110,23 +137,26 @@ function DayTasks({ tasks }) {
 					<div className="inner-newdate-lists">
 						<ul>
 							{task.weekdayTasks.map((weekdayTask) => (
-								<TaskLists key={weekdayTask.taskId} task={weekdayTask} />
+								<TaskLists key={weekdayTask.taskId} task={weekdayTask} setAllTasks={setTasks} />
 							))}
 						</ul>
 					</div>
 				</div>
 			))}
-		</div>
+		</>
 	);
 }
 
-function TaskLists({ task }) {
+function TaskLists({ task, setAllTasks }) {
 	let [text, setText] = useState(task.text);
-	let [isEditable, setIsEditable] = useState(false);
-	let strike = false;
+	let [isEditable, setIsEditable] = useState(task.isEditable);
+	let [strike, setStrike] = useState(task.strike);
+	let ogText = task.text;
+	let changabletext = text;
 	let textVal = "";
+
 	function toggleStrikeThrough() {
-		strike = !strike;
+		setStrike(!strike);
 	}
 
 	function handleEditableClick() {
@@ -134,43 +164,76 @@ function TaskLists({ task }) {
 	}
 
 	function cancelEdit() {
-		setText(task.text);
+		setText((text) => {
+			text = ogText;
+			return text;
+		});
 		setIsEditable(false);
 	}
 
 	function saveEdit() {
-		const updatedTask = { ...task, text: textVal };
-
+		setText((text) => {
+			text = changabletext;
+			task.text = changabletext;
+			return text;
+		});
 		setIsEditable(false);
-
-		setText(updatedTask.text);
 	}
 
 	function handleTextChange(event) {
-		textVal = event.target.value;
-		setText(textVal);
+		textVal += event.target.value;
+		console.log(textVal);
+		setText((text) => {
+			text = textVal;
+			return text;
+		});
+	}
+
+	function handleDeleteClick() {
+		setAllTasks(() => {
+			let tempTaskArr = JSON.parse(localStorage.getItem("tasks"));
+			tempTaskArr = tempTaskArr.map((singletask) => {
+				if (singletask.id === task.id) {
+					if (singletask.weekdayTasks.length === 0) {
+						return null;
+					} else {
+						return {
+							...singletask,
+							weekdayTasks: singletask.weekdayTasks.filter((weekdayTask) => {
+								return weekdayTask.taskId !== task.taskId;
+							}),
+						};
+					}
+				}
+				return singletask;
+			});
+			localStorage.setItem("tasks", JSON.stringify(tempTaskArr));
+			return tempTaskArr;
+		});
 	}
 
 	return (
 		<li>
 			<div className="radio-parent">
-				<div className="radio-btns" onClick={toggleStrikeThrough}></div>
+				<div className={`radio-btns ${strike ? "radio-btns-glow" : ""}`} onClick={toggleStrikeThrough}></div>
 				<div>{task.time}</div>
 			</div>
 			<div className="added-list">
 				{!isEditable ? (
 					<div className="centerText">
-						<p className={`added ${strike ? "strike-through" : ""}`}>{task.text}</p>
+						<p className={`added ${strike ? "strike-through" : ""}`}>{text}</p>
 					</div>
 				) : (
 					<div className="centerText">
-						<textarea className="added editable" value={text} onChange={handleTextChange} />
+						<textarea className="added editable" onInput={handleTextChange}>
+							{text}
+						</textarea>
 					</div>
 				)}
 				<div className="popup">
 					{!isEditable && (
 						<>
-							<i className="ri-delete-bin-2-line"></i>
+							<i className="ri-delete-bin-2-line" id="deletebtn" onClick={handleDeleteClick}></i>
 							<i className="ri-edit-fill" id="editbtn" onClick={handleEditableClick}></i>
 						</>
 					)}
