a0b3a527c0b0f172ac9f61a5581443477db0fe93
RiteshPandit <87899750+Riteshp2001@users.noreply.github.com>
2023-09-23 17:24:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
   };
 
   const handleAddTask = (taskText, day) => {
+    if(taskText==="")return;
     const currentDate = new Date().toLocaleDateString();
     const newTask = {
       day: currentDate,
